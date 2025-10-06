be20228e7636a0a7ef96f67ee20b3b386d1bf42c
timote11000-cmd <timote11000@gmail.com>
2025-10-06 12:59:44 +0200

    phase de teste et ecriture gaulois

--- src/personnages/Gaulois.java
+++ src/personnages/Gaulois.java
@@ -1,5 +1,21 @@
 package personnages;
 
 public class Gaulois {
-
+	private String nom;
+	private int force;
+	public Gaulois(String nom, int force) {
+		super();
+		this.nom = nom;
+		this.force = force;
+	}
+	public String getNom() {
+		return nom;
+	}
+	private void parler(String texte) {
+		System.out.println(prendreParole() + "\"" + texte + "\"");
+	}
+	private String prendreParole() {
+		return "Le gaulois " + nom + " : ";
+	}
+	
 }
